768816c1d18925b26b8c83561d2642e5260f831b
hanguangmic <45691194+hanguangmic@users.noreply.github.com>
2021-02-04 09:19:35 +0800

    1
    1

---
+++ HRNet-Semantic-Segmentation/lib/utils/kidney_loss_cal.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+
+
+class SoftDiceLoss(nn.Module):
+    def __init__(self, smooth=1., dims=(-2, -1)):
+
+        super(SoftDiceLoss, self).__init__()
+        self.smooth = smooth
+        self.dims = dims
+
+    def forward(self, x, y):
+
+        tp = (x * y).sum(self.dims)
+        fp = (x * (1 - y)).sum(self.dims)
+        fn = ((1 - x) * y).sum(self.dims)
+
+        dc = (2 * tp + self.smooth) / (2 * tp + fp + fn + self.smooth)
+        dc = dc.mean()
+
+        return 1 - dc
